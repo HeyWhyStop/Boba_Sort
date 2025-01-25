cfab9ff9632bb2ad2e83f3aa21d23ac5761a3e80
Jayden Trinh <jhtrinh24@gmail.com>
2025-01-25 14:37:16 -0800

    Testing

---
+++ Boba-Game/button.gd
@@ -0,0 +1,20 @@
+extends AnimatableBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var up = true
+
+
+func _on_button_pressed() -> void:
+	if up:
+		animation_player.play("move_down")
+	if not up:
+		animation_player.play("move_up")
+
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "move_down":
+		up = false
+	if anim_name == "move_up":
+		up = true
+	pass # Replace with function body.
