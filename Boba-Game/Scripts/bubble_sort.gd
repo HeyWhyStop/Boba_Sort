a9026f9b3eb11ae20d51955f34ae001eb56958f8
lukes4 <60821484+HeyWhyStop@users.noreply.github.com>
2025-01-25 17:25:17 -0500

    started making bubble sort towers
    made a function that creates duplicates one after the other

--- Boba-Game/Scripts/bubble_sort.gd
+++ Boba-Game/Scripts/bubble_sort.gd
@@ -2,18 +2,29 @@ extends Node
 
 @export var size = 5
 
-var bubble_arr = []
+var size_arr = []
+var bubble_tower_base = load("res://Scenes/bubble_tower_test.tscn");
+var bbl_arr = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#bubble_tower_base = self.get_child(0)
+	#print(bubble_tower_base.name)
 	var random = RandomNumberGenerator.new()
-	while(bubble_arr.size() < size):
+	while(size_arr.size() < size):
 		var rand_int = random.randi_range(1, size)
-		if(bubble_arr.find(rand_int) < 0):
-			bubble_arr.append(rand_int)
+		if(size_arr.find(rand_int) < 0):
+			size_arr.append(rand_int)
+			var child = bubble_tower_base.instantiate()
+			child.position.x = self.position.x
+			child.position.y = self.position.y
+			bbl_arr.append(child)
+			self.add_child(bbl_arr[-1])
+			self.position.x += 200
+			print(bbl_arr[-1].name)
 	
-	for i in bubble_arr:
+	for i in size_arr:
 		print(i);
 	pass # Replace with function body.
 
--- Boba-Game/Scripts/bubble_tower_.gd
+++ Boba-Game/Scripts/bubble_tower_.gd
@@ -1,8 +1,12 @@
 extends Node
 
+#var parent = self.get_parent().get_script()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(self.name)
+	#var child_node = self.get_child(0)
+	#print(child_node.name)
 	pass # Replace with function body.
 
 
