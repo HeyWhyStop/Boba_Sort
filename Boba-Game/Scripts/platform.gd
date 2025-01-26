31b772c64f7b6480f5661b193e38e979a2aa2ce7
Ishaan-Mit <Ishaan.Mittal07@gmail.com>
2025-01-26 11:35:26 -0800

    TWEENS

--- Boba-Game/Scripts/platform.gd
+++ Boba-Game/Scripts/platform.gd
@@ -4,32 +4,35 @@ extends Node2D
 @export var y_move: int = 0
 @export var time: float = 1
 
+#@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var platform: AnimatableBody2D = $AnimatableBody2D
+
 var start_pos: bool = true
 var moving: bool = false
+var tween = null
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Button1.position = Vector2(0-24,0-24)
+	$Button1.position = Vector2(0-24, 0-24)
 	$Button2.position = Vector2(x_move-24, y_move-24)
-	var animation = animation_player.get_animation("move")
-	animation.length = time
-	animation.track_insert_key(0, 0, Vector2(0, 0))
-	animation.track_insert_key(0, time, Vector2(x_move, y_move))
+	var tween = get_tree().create_tween()
+	pass
+
 
 func _on_button_1_pressed() -> void:
 	if not start_pos and not moving:
 		start_pos = true
 		moving = true
-		animation_player.play_backwards("move")
+		tween = get_tree().create_tween()
+		tween.tween_property(platform, "position", Vector2(0, 0), time).from(Vector2(x_move, y_move))
+		await tween.finished
+		moving = false
+
 
 func _on_button_2_pressed() -> void:
 	if start_pos and not moving:
 		start_pos = false
 		moving = true
-		animation_player.play("move")
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "move":
+		tween = get_tree().create_tween()
+		tween.tween_property(platform, "position", Vector2(x_move, y_move), time).from(Vector2(0, 0))
+		await tween.finished
 		moving = false
